6b9d5cc319752b4c7d9797fb039b1c7b4121b52e
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-04-02 10:57:33 +0200

    feat: ✨ Add Cargo.toml and allow changing name of project

--- rust-training-esp32c3/compile.sh
+++ rust-training-esp32c3/compile.sh
@@ -3,17 +3,18 @@
 set -e
 
 cd espressif-trainings/intro/hardware-check
-if [ -f ${HOME}/build-in/main.rs ]; then
-    cat ${HOME}/build-in/main.rs >src/main.rs
+if [ "$(find ${HOME}/build-in -name '*.rs')" ]; then
+    cp ${HOME}/build-in/*.rs src
 fi
 
-if [ -f ${HOME}/build-in/lib.rs ]; then
-    cat ${HOME}/build-in/lib.rs >src/lib.rs
+if [ -f ${HOME}/build-in/Cargo.toml ]; then
+    cp ${HOME}/build-in/Cargo.toml Cargo.toml
+    PROJECT_NAME=$(awk -F= '/name/ {gsub(/^[[:space:]]+|['\''"]|[[:space:]]+$/,"",$2); print $2}' Cargo.toml)
+    PROJECT_NAME_UNDERSCORE=${PROJECT_NAME//-/_}
+    find . -type f -exec sed -i "s/$PROJECT_NAME/hardware-check/g" {} +
+    find . -type f -exec sed -i "s/$PROJECT_NAME_UNDERSCORE/hardware_check/g" {} +
 fi
 
-if [ -f ${HOME}/build-in/imc42670p.rs ]; then
-    cat ${HOME}/build-in/imc42670p.rs >src/imc42670p.rs
-fi
 cargo build --offline --release
 python3 -m esptool --chip ${WOKWI_MCU} elf2image --flash_size 4MB target/riscv32imc-esp-espidf/release/hardware-check -o ${HOME}/build-out/project.bin
 cp target/riscv32imc-esp-espidf/release/hardware-check ${HOME}/build-out/project.elf
